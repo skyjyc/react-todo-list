dcee4cc7d1e34ad3f55dbf6000b88e28bac93aef
Li Kun <603469018@qq.com>
2021-03-27 10:45:13 +0800

    修改为Hook方式

--- src/App.js
+++ src/App.js
@@ -1,99 +1,166 @@
-
-import React from 'react';
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchVal: '',
-      list:[
+import React,{useState} from 'react';
+function App(props) {
+    let [searchVal,setSearchVal] = useState('');
+    let [list,setList] = useState([
         {name:'React',id:1,show:true},
         {name: 'Vue',id: 2,show:true},
         {name: 'AG',id: 3,show:true}
-      ]
+    ])
+    console.log(list,setSearchVal,"listInit")
+    function handleInputChange(e) {
+        setSearchVal(e.target.value)
     }
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-  handleInputChange(e) {
-   this.setState({ 
-    searchVal: e.target.value
-   })
-  }
-  handleSearch(e) {
-    e.preventDefault();
-    let listTemp = this.state.list.slice();
-    let reg = this.state.searchVal? new RegExp(this.state.searchVal) : false;
-    for(let i in listTemp) {
-      const item = listTemp[i];
-      if(!reg || reg.test(item.name)) {
-        item.show = true
-      }else {
-        item.show = false
-      }
-    }
-    this.setState({
-      list: listTemp
-    })
-  }
-  handleAdd(e) { 
-    e.preventDefault();
-    if(!this.state.searchVal) {
-      alert('添加时内容不能为空')
-      return
-    }
-    this.setState(
-      (state)=>{ 
-        const listTemp = state.list.slice()
-        listTemp.push({name: state.searchVal,id: state.list[state.list.length - 1].id + 1,show: true})
-        return {
-          list: listTemp
+    function handleSearch(e) {
+        e.preventDefault();
+        let listTemp = JSON.parse(JSON.stringify(list));
+        let reg = searchVal ? new RegExp(searchVal) : false;
+        for(let i in listTemp) {
+            const item = listTemp[i];
+            if(!reg || reg.test(item.name)) {
+                item.show = true
+            }else {
+                item.show = false
+            }
         }
-      }
-    )
-  }
-  handleEdit(id) {
-    if(!this.state.searchVal) {
-      alert('添加时内容不能为空')
-      return
+        setList(listTemp)    
     }
-    let  listTemp = this.state.list.slice();
-    listTemp[id - 1].name = this.state.searchVal
-    this.setState({
-      list: listTemp
-    })
-  }
-  handleDelete(id) {
-    let  listTemp = this.state.list.slice();
-    listTemp.splice(id - 1,1);
-    for(let x in listTemp) {
-      const item = listTemp[x];
-      item.id = Number(x) + 1
+    function handleAdd(e) { 
+         e.preventDefault();
+         if(!searchVal) {
+           alert('添加时内容不能为空')
+           return
+         }
+         let listTemp = JSON.parse(JSON.stringify(list));
+         listTemp.push({name: searchVal,id: list[list.length - 1].id + 1,show: true});
+         setList(listTemp);
     }
-    console.log(listTemp,'listTemp')
-    this.setState((state)=>({
-      list : listTemp
-    }))
-  }
-  render() {
+    function handleEdit(id) {
+        if(!searchVal) {
+           alert('修改内容不能为空')
+           return
+        }
+        let  listTemp = JSON.parse(JSON.stringify(list));
+        listTemp[id - 1].name = searchVal
+        setList(listTemp)
+    }
+    function handleDelete(id) {
+         let  listTemp = JSON.parse(JSON.stringify(list));
+         listTemp.splice(id - 1,1);
+         for(let x in listTemp) {
+           const item = listTemp[x];
+           item.id = Number(x) + 1
+         }
+         console.log(listTemp,'listTemp')
+         setList(listTemp);
+    }
     return (
-      <div className="App">
-        <h3>
-          TodoList
-        </h3>
-        <SearchBar searchVal={this.state.searchVal} 
-          handleInputChange={this.handleInputChange} 
-          handleAdd={this.handleAdd}
-          handleSearch={this.handleSearch}
-          ></SearchBar>
-        <TodoList list={this.state.list} handleDelete={this.handleDelete} handleEdit={this.handleEdit}></TodoList>
-      </div>
-    )
-
-  };
+        <div className="App">
+          <h3>
+            TodoList
+          </h3>
+          <SearchBar searchVal={searchVal} 
+            handleInputChange={handleInputChange} 
+            handleAdd={handleAdd}
+            handleSearch={handleSearch}
+            ></SearchBar>
+          <TodoList list={list} handleDelete={handleDelete} handleEdit={handleEdit}></TodoList>
+        </div>
+      )
 }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       searchVal: '',
+//       list:[
+//         {name:'React',id:1,show:true},
+//         {name: 'Vue',id: 2,show:true},
+//         {name: 'AG',id: 3,show:true}
+//       ]
+//     }
+//     this.handleInputChange = this.handleInputChange.bind(this);
+//     this.handleAdd = this.handleAdd.bind(this);
+//     this.handleDelete = this.handleDelete.bind(this);
+//     this.handleEdit = this.handleEdit.bind(this);
+//     this.handleSearch = this.handleSearch.bind(this);
+//   }
+//   handleInputChange(e) {
+//    this.setState({ 
+//     searchVal: e.target.value
+//    })
+//   }
+//   handleSearch(e) {
+//     e.preventDefault();
+//     let listTemp = this.state.list.slice();
+//     let reg = this.state.searchVal? new RegExp(this.state.searchVal) : false;
+//     for(let i in listTemp) {
+//       const item = listTemp[i];
+//       if(!reg || reg.test(item.name)) {
+//         item.show = true
+//       }else {
+//         item.show = false
+//       }
+//     }
+//     this.setState({
+//       list: listTemp
+//     })
+//   }
+//   handleAdd(e) { 
+//     e.preventDefault();
+//     if(!this.state.searchVal) {
+//       alert('添加时内容不能为空')
+//       return
+//     }
+//     this.setState(
+//       (state)=>{ 
+//         const listTemp = state.list.slice()
+//         listTemp.push({name: state.searchVal,id: state.list[state.list.length - 1].id + 1,show: true})
+//         return {
+//           list: listTemp
+//         }
+//       }
+//     )
+//   }
+//   handleEdit(id) {
+//     if(!this.state.searchVal) {
+//       alert('添加时内容不能为空')
+//       return
+//     }
+//     let  listTemp = this.state.list.slice();
+//     listTemp[id - 1].name = this.state.searchVal
+//     this.setState({
+//       list: listTemp
+//     })
+//   }
+//   handleDelete(id) {
+//     let  listTemp = this.state.list.slice();
+//     listTemp.splice(id - 1,1);
+//     for(let x in listTemp) {
+//       const item = listTemp[x];
+//       item.id = Number(x) + 1
+//     }
+//     console.log(listTemp,'listTemp')
+//     this.setState((state)=>({
+//       list : listTemp
+//     }))
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h3>
+//           TodoList
+//         </h3>
+//         <SearchBar searchVal={this.state.searchVal} 
+//           handleInputChange={this.handleInputChange} 
+//           handleAdd={this.handleAdd}
+//           handleSearch={this.handleSearch}
+//           ></SearchBar>
+//         <TodoList list={this.state.list} handleDelete={this.handleDelete} handleEdit={this.handleEdit}></TodoList>
+//       </div>
+//     )
+
+//   };
+// }
 function SearchBar(props) {
  return (
    <div>
@@ -108,14 +175,15 @@ function SearchBar(props) {
    </div>
  )
 }
+
 function TodoList(props) {
-  const filteredArr = props.list.filter((item)=>{
+  const filteredArr =props.list ? props.list.filter((item)=>{
     if(item.show) {
       return true
     }else {
       return false
     }
-  })
+  }) : []
   console.log(filteredArr,"filtered")
   return (
     <ul>
@@ -134,19 +202,19 @@ function TodoList(props) {
     </ul>
   )
 }
-class ListItem extends React.Component
+function ListItem(props)
  {
-  handleDelete(id) {
-    this.props.handleDelete(id)
+  function handleDelete(id) {
+    props.handleDelete(id)
   }
-  handleEdit(id) {
-    this.props.handleEdit(id)
+  function handleEdit(id) {
+    props.handleEdit(id)
   }
-  render() {
+  
     return  (
-      <li>{this.props.name}<button onClick={()=>{this.handleDelete(this.props.id)}}>删除</button><button onClick={()=>{this.handleEdit(this.props.id)}}>修改</button></li>
+      <li>{props.name}<button onClick={()=>{handleDelete(props.id)}}>删除</button><button onClick={()=>{handleEdit(props.id)}}>修改</button></li>
     )
-  }
+  
 
     
 }
